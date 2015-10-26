7444ba0fa4c5edfce9301f62f2a9db01e155cd4c
Kelly Ripple <kelly.m.ripple@gmail.com>
2015-10-25 23:16:00 -0400

    WIP

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,20 +1,31 @@
 class ContactsController < ApplicationController
-	include Parser
-	
 	def index
 		@contacts = Contact.all
 	end
 
 	def new
+		@contact = Contact.new
 	end
 
 	def create
+		file = params[:contact][:file].tempfile
+		data = SmarterCSV.process(file, :col_sep => "\t")
+		data.each do |contact_attributes|
+			@contact = Contact.create(contact_attributes)
+		end
+		redirect_to :root
 	end
 
 	def destroy
+		if request.xhr?
+			binding.pry
+		end
 	end
 
 	def filter
+		if request.xhr?
+			binding.pry
+		end
 	end
 
 	private
@@ -22,3 +33,4 @@ class ContactsController < ApplicationController
 	def contact_params
 	end
 end
+
--- app/models/parser.rb
+++ app/models/parser.rb
@@ -1,18 +1,49 @@
-module Parser
-	def import(file)
-    data = CSV.read(file).to_a
-    # CSV.read returns a CSV table that's in the format of an array of arrays & the first row is the headers. Need to convert to array to use array methods
-    data.map do |row|
-      row
-    end
-  end
+# module Parser
 
-  def save(file, data)
-    CSV.open(file,"w") do |csv|
-      data.each do |row|
-        csv << [row] # takes arrays only. CSV needs to know where the comma column separators go
-      end
-    end
-    #File.write(file, new_file)
-  end
-end
+#   extend self
+
+#   def get_attributes(file)
+#     data = CSV.read(file).to_a
+#     binding.pry 
+#     headers = data.pop
+#     binding.pry
+#     data.map do |row|
+#       binding.pry
+#       row
+#     end
+
+#     # :first_name
+#     # :last_name
+#     # :email_address
+#     # :phone_number
+#     # :company_name
+#     # return attributes
+#   end
+
+
+# 	def import(file)
+#     data = CSV.read(file).to_a
+#     # CSV.read returns a CSV table that's in the format of an array of arrays & the first row is the headers. Need to convert to array to use array methods
+#     headers = data.pop # probably?
+#     data.map do |row|
+#       row
+#     end
+#   end
+
+#   def save(file, data)
+#     CSV.open(file,"w") do |csv|
+#       data.each do |row|
+#         csv << [row] # takes arrays only. CSV needs to know where the comma column separators go
+#       end
+#     end
+#     #File.write(file, new_file)
+#   end
+# end
+
+
+
+
+
+
+
+
