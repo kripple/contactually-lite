6ce9ad80ce6c995695b02b83f0514e2fb85211c9
Kelly Ripple <kelly.m.ripple@gmail.com>
2015-10-26 05:28:29 -0400

    add email ordering

--- app/assets/javascripts/contacts.js
+++ app/assets/javascripts/contacts.js
@@ -33,36 +33,57 @@ function initFilterContacts() {
 		});
 	});
 
-	// $(".filter-extensions").on('click', function(e){
-	// 	e.preventDefault();
-	// 	var url = $(this).attr('href');
-	// 	var container = $('.contacts-container');
-	// 	var contacts = $(".contact");
-	// 	contacts.each(function(){
-	// 		$(this).children().children(".international")
-	// 	});
-	// });
+	$(".filter-extensions").on('click', function(e){
+		e.preventDefault();
+		var contacts = $(".contact");
+		contacts.each(function(){
+			$(this).show();
+			var value = $(this).children().children(".phone").html();
+			if(value.length <= 13){
+				$(this).hide();
+			};
+		});
+	});
 
-	// $(".filter-dot-com").on('click', function(e){
-	// 	e.preventDefault();
-	// 	var url = $(this).attr('href');
-	// 	var container = $('.contacts-container');
-	// 	var contacts = $(".contact");
-	// 	contacts.each(function(){
-	// 		$(this).children().children(".international")
-	// 	});
-	// });
+	$(".filter-dot-com").on('click', function(e){
+		e.preventDefault();
+		var contacts = $(".contact");
+		contacts.each(function(){
+			$(this).show();
+			var value = $(this).children().children(".email").html();
+			if(!value.includes(".com")){
+				$(this).hide();
+			};
+		});
+	});
 
-	// $(".filter-email-order").on('click', function(e){
-	// 	e.preventDefault();
-	// 	var url = $(this).attr('href');
-	// 	var container = $('.contacts-container');
-	// 	var contacts = $(".contact");
-	// 	contacts.each(function(){
-	// 		$(this).children().children(".international")
-	// 	});
-	// });
-
+	$(".filter-email-order").on('click', function(e){
+		e.preventDefault();
+		var contacts = $(".contact");
+		var emails = [];
+		contacts.each(function(){
+			$(this).show();
+			var value = $(this).children().children(".email").html();
+			emails.push(value);
+		});
+		emails.sort()
+		
+		for(var i = 0 ; i < emails.length ; i++ ){
+			contacts.each(function(){
+				var value = $(this).children().children(".email").html();
+				if(value == emails[i]){
+					$(".contacts-container").append($(this));
+					return false;
+				};
+			});
+		};
+	});
 };
+
+
+
+
+
+
 
 
