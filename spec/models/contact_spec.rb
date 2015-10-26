f679ed885e71c6e33ab365ccda36d7f5a549dd07
Kelly Ripple <kelly.m.ripple@gmail.com>
2015-10-26 08:28:47 -0400

    more styling

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,37 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
 
-  # make up 3-5 examples
-
-  # describe '#full_name' do 
-  #   it 'should return true if current cafe is logged in' do 
-  #     expect(cafe.owner?(cafe)).to be_truthy
-  #   end  
-  # end
-
-  # describe '#display_phone_number' do 
-  #   it 'should return true if current cafe is logged in' do 
-  #     expect(cafe.owner?(cafe)).to be_truthy
-  #   end  
-  # end
-
-  # describe '#owner?' do 
-  #   it 'should return true if current cafe is logged in' do 
-  #     expect(cafe.owner?(cafe)).to be_truthy
-  #   end  
-  # end
-
-  # describe '#owns_item?' do 
-  #   it 'should return true for a menu belonging to a particular cafe' do 
-  #     item = cafe.menu_items.create(FactoryGirl.attributes_for(:menu_item))
-  #     expect(cafe.owns_item?(item)).to be_truthy
-  #   end 
-  # end  
-
-  # describe '#owner?' do 
-  #   it 'should return true if current cafe is logged in' do 
-  #     expect(cafe.owner?(cafe)).to be_truthy
-  #   end  
-  # end 
+  pending
 
 end
